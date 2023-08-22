943ee9531ad6c4b2bf72199fbc3e379c1a7b9085
clark <clark.tabar@synacy.com>
2023-08-23 00:53:52 +0800

    Added add-employee user interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HrAdminComponent} from "./hr-admin/hr-admin.component";
+import {AddEmployeeComponent} from "./hr-admin/add-employee/add-employee.component";
 
 const routes: Routes = [
   {
     path: 'hr-admin',
     children:[
-      {path: '', component: HrAdminComponent}
+      {path: '', component: HrAdminComponent},
+      {path: 'add-employee', component: AddEmployeeComponent}
     ]
-  }
+  },
+  {path: '**', redirectTo:'hr-admin'}
 ];
 
 @NgModule({
