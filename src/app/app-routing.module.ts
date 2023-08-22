a900d5b8685b3f75df022e2bc0d20d498811ff63
clark <clark.tabar@synacy.com>
2023-08-23 02:13:12 +0800

    Added add-employee and edit-employee implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HrAdminComponent} from "./hr-admin/hr-admin.component";
 import {AddEmployeeComponent} from "./hr-admin/add-employee/add-employee.component";
+import {EditEmployeeComponent} from "./hr-admin/edit-employee/edit-employee.component";
 
 const routes: Routes = [
   {
     path: 'hr-admin',
     children:[
       {path: '', component: HrAdminComponent},
-      {path: 'add-employee', component: AddEmployeeComponent}
+      {path: 'add-employee', component: AddEmployeeComponent},
+      {path: 'edit-employee', component: EditEmployeeComponent}
     ]
   },
   {path: '**', redirectTo:'hr-admin'}
