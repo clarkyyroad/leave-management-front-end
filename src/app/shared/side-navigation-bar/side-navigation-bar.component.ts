c1e077fddcf54adf045dbe65bd9f121baaf6cd6c
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-27 00:52:44 +0800

    Added a method to initialize the list of employee

--- src/app/shared/side-navigation-bar/side-navigation-bar.component.ts
+++ src/app/shared/side-navigation-bar/side-navigation-bar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {IEmployeeList} from "../../hr-admin/model/employee-list.model";
+import {EmployeeService} from "../../hr-admin/service/employee.service";
+import {RouterService} from "../../hr-admin/service/router.service";
 
 @Component({
   selector: 'app-side-navigation-bar',
   templateUrl: './side-navigation-bar.component.html',
   styleUrls: ['./side-navigation-bar.component.css']
 })
-export class SideNavigationBarComponent {
+export class SideNavigationBarComponent implements OnInit {
 
+  public employeeList: IEmployeeList[] = [];
+
+  constructor(private employeeService: EmployeeService, private routerService: RouterService) {
+  }
+
+  ngOnInit() {
+    this.initializeListEmployees();
+  }
+
+  private initializeListEmployees() {
+    this.employeeService.getEmployeeList().subscribe({
+      next: (data: IEmployeeList[]) => {
+        this.employeeList = data;
+        console.log('Response:', data);
+      }
+    });
+  }
 }
