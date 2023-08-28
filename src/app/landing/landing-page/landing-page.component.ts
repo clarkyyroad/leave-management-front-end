625170898ac12b19fbeb3377946d732d0346c7cf
BlankDesu13 <romeo.fermano@synacy.com>
2023-08-28 17:37:08 +0800

    Added user navigation method

--- src/app/landing/landing-page/landing-page.component.ts
+++ src/app/landing/landing-page/landing-page.component.ts
@@ -1,39 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RoleType} from "../../shared/side-navigation-bar/role-type.enum";
 import {RouterService} from "../../service/router.service";
+import {IUsers} from "../../hr-admin/model/employee-list.model";
+import {LandingPageService} from "../services/landing-page.service";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
 
   public member: RoleType = RoleType.MEMBER
   public manager: RoleType = RoleType.MANAGER
   public admin: RoleType = RoleType.ADMIN
+  public users: IUsers[] = [];
+  public selectedUser: string = '';
 
-
-  public user1 = "Dexter Sy"
-  public user2 = "Romeo"
-  public user3 = "Clark"
-  public user4 = "Carli"
-
-
-
-  constructor(private routerService: RouterService) {
+  constructor(private routerService: RouterService, private landingPageService: LandingPageService) {
   }
 
-  public getAdminPath(){
-    this.routerService.navigate('/hr-admin/', {'id': 1})
+  ngOnInit() {
+    this.initializeUser();
   }
 
-  public getManagerPath(){
-    this.routerService.navigate('/manager/', {'id': 1})
-  }
-  public getMemberPath(){
-    this.routerService.navigate('/member/', {'id': 1})
+  private initializeUser() {
+    this.landingPageService.getUser().subscribe({
+      next: (data: IUsers[]): void => {
+        this.users = data;
+        console.log('Response: ', data)
+      }
+    });
   }
 
-
+  public getUserPath(): void{
+    if (this.selectedUser === 'HR_ADMIN') {
+      this.routerService.navigate('/hr-admin/').then(() => console.log('Navigation successful'))
+          .catch((error) => console.error('Navigation error: ', error));
+    } else if (this.selectedUser === 'MANAGER') {
+      this.routerService.navigate('/manager/').then(() => console.log('Navigation successful'))
+          .catch((error) => console.error('Navigation error: ', error));
+    } else {
+      this.routerService.navigate('/member/').then(() => console.log('Navigation successful'))
+          .catch((error) => console.error('Navigation: ', error));
+    }
+  }
 }
